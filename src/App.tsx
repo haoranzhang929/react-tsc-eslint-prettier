f952fdf9744c9c87a5ffdd0cb576080651f758ee
Haoran <haoranzhang@icloud.com>
2021-11-20 15:11:23 +0000

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div style={{ display: "flex", height: "100vh", justifyContent: "center" }}>
+      <h1 style={{ height: "fit-content", alignSelf: "center", width: "calc(100vw - 50%)", textAlign: "center" }}>
+        React typescript template with eslint + prettier + tsc configuration
+      </h1>
+    </div>
+  );
+};
+
+export default App;
